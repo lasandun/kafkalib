f083612c1921a86fffd2ea6bf59182a4b805eb0d
Lahiru Lasandun <khlahiru.lasandun@incognito.com>
2025-08-03 19:34:29 +0530

    Added ProducerWrapper

---
+++ pkg/producer/producer.go
@@ -0,0 +1,54 @@
+package kafka
+
+import (
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+type ProducerWrapper struct {
+	producer *kafka.Producer
+}
+
+func NewProducerWrapper(broker string) *ProducerWrapper {
+	var producerWrapper ProducerWrapper
+	var err error = nil
+
+	producerWrapper.producer, err = kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": broker})
+	if err != nil {
+		fmt.Println("failed to create the producer", err)
+		return nil
+	}
+
+	return &producerWrapper
+}
+
+func (p *ProducerWrapper) Produce(topic string, message []byte) error {
+	if p.producer == nil {
+		return fmt.Errorf("producer is not created")
+	}
+
+	deliveryChan := make(chan kafka.Event, 1)
+
+	err := p.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          message,
+	}, deliveryChan)
+
+	if err != nil {
+		return err
+	}
+
+	e := <-deliveryChan
+	m := e.(*kafka.Message)
+
+	if m.TopicPartition.Error != nil {
+		return m.TopicPartition.Error
+	}
+
+	return nil
+}
+
+func (p *ProducerWrapper) Close() {
+	p.producer.Close()
+}
---
+++ pkg/producer/producer_test.go
@@ -0,0 +1,22 @@
+package kafka_test
+
+import (
+	"testing"
+
+	kafka "github.com/lasandun/kafkalib/pkg/producer"
+)
+
+func TestProduce(t *testing.T) {
+	producerWrapper := kafka.NewProducerWrapper("localhost:9092")
+
+	if producerWrapper == nil {
+		t.Fatalf("Failed to initialize producer")
+	}
+
+	err := producerWrapper.Produce("test1", []byte("test message"))
+	if err != nil {
+		t.Fatalf("Failed to produce message: %v", err)
+	}
+
+	producerWrapper.Close()
+}
